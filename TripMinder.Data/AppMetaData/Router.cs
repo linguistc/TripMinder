be847f298a279c29c5bea3d4b1138be56a085430
ahmedsameh <ahmadsameh1@proton.me>
2025-03-03 07:12:38 +0200

    initial custom routing

---
+++ TripMinder.Data/AppMetaData/Router.cs
@@ -0,0 +1,58 @@
+﻿namespace TripMinder.Data.AppMetaData
+{
+    public static class Router
+    {
+        public const string SingleRouter = "{id}/";
+        public const string root = "Api/";
+        public const string version = "v1/";
+
+        public const string Rule = root + version;
+
+
+        public static class RestaurantRouting
+        {
+            public const string Prefix = Rule + "restaurant/";
+            public const string List = Prefix + "list/";
+            public const string GetById = Prefix + SingleRouter;
+
+            public const string Create = Prefix + "create/";
+            public const string Edit = Prefix + "edit/";
+            public const string Delete = Prefix + SingleRouter;
+
+            public const string Paginate = Prefix + "paginate/";
+        }
+
+        public static class AccomodataionRouting
+        {
+            public const string Prefix = Rule + "accommodation/";
+            public const string List = Prefix + "list/";
+            public const string GetById = Prefix + SingleRouter;
+            public const string Create = Prefix + "create/";
+            public const string Edit = Prefix + "edit/";
+            public const string Delete = Prefix + SingleRouter;
+            public const string Paginate = Prefix + "paginate/";
+        }
+
+        public static class EntertainmentRouting
+        {
+            public const string Prefix = Rule + "entertainment/";
+            public const string List = Prefix + "list/";
+            public const string GetById = Prefix + SingleRouter;
+            public const string Create = Prefix + "create/";
+            public const string Edit = Prefix + "edit/";
+            public const string Delete = Prefix + SingleRouter;
+            public const string Paginate = Prefix + "paginate/";
+        }
+
+        public static class TourismAreaRouting
+        {
+            public const string Prefix = Rule + "tourismarea/";
+            public const string List = Prefix + "list/";
+            public const string GetById = Prefix + SingleRouter;
+            public const string Create = Prefix + "create/";
+            public const string Edit = Prefix + "edit/";
+            public const string Delete = Prefix + SingleRouter;
+            public const string Paginate = Prefix + "paginate/";
+        }
+    }
+}
