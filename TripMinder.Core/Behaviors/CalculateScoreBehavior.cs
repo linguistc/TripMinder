ebff43216dde10fe8db9dfa00cd9a78ce0dc6d27
ahmedsameh <ahmadsameh1@proton.me>
2025-04-22 19:06:25 +0200

    Renaming suggestercontroller

--- TripMinder.Core/Behaviors/CalculateScoreBehavior.cs
+++ TripMinder.Core/Behaviors/CalculateScoreBehavior.cs
@@ -4,7 +4,7 @@ public static class CalculateScoreBehavior
 {
     public static float CalculateScore(string classType, int priority, double averagePricePerAdult)
     {
-        float classWeight = classType == "A" ? 23 : classType == "B" ? 17 : classType == "C" ? 11 : 3;
+        float classWeight = classType == "A" ? 4 : classType == "B" ? 3 : classType == "C" ? 2 : 1;
         // float priceFactor = (float)(averagePricePerAdult / 1000); // التكلفة بتساهم في السكور
         return classWeight * priority;
     }
